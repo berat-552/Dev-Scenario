e52243aa3baa5587a69223c7783504964197a02a
berat-552 <berat.d7599@gmail.com>
2025-09-27 23:41:43 +0100

    add logos seed

---
+++ Sql/NBA_SeedLogos.sql
@@ -0,0 +1,34 @@
+﻿USE NBA;
+GO
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/229/full/8926_denver_nuggets-primary-2019.png'
+WHERE Name = 'Denver Nuggets';
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/221/full/5114_chicago_bulls-primary_on_dark-1967.png'
+WHERE Name = 'Chicago Bulls';
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/230/full/6830_houston_rockets-primary-2020.png'
+WHERE Name = 'Houston Rockets';
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/224/full/indiana-pacers-logo-primary-2018-7753.png'
+WHERE Name = 'Indiana Pacers';
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/214/full/burm5gh2wvjti3xhei5h16k8e.gif'
+WHERE Name = 'Miami Heat';
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/2687/full/oklahoma-city-thunder-logo-primary-2009-9699.png'
+WHERE Name = 'Oklahoma City Thunder';
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/233/full/2547_san_antonio_spurs-primary-2018.png'
+WHERE Name = 'San Antonio Spurs';
+
+UPDATE dbo.Teams
+SET Logo = 'https://content.sportslogos.net/logos/6/227/full/7024_toronto_raptors-primary-2021.png'
+WHERE Name = 'Toronto Raptors';
